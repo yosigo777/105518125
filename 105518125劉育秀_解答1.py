5a5ee334475da6232fa46a314d5e0aba9a8f6f9f
yosigo777 <103197348+yosigo777@users.noreply.github.com>
2022-04-16 12:43:38 +0800

    Add files via upload

---
+++ 105518125劉育秀_解答1.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 13 22:59:44 2022
+
+@author: 105518125劉育秀
+
+作業一:
+1
+22
+333
+4444
+55555
+"""
+for i in range(1,6): #[1,2,3,4,5]
+    for x in range(i): #i是多少　就執行幾次
+        print(i,end="")
+    print()
+    
+print("-"*30)
+
+
+
+
+
+
+
+
+
+
