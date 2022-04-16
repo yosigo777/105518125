65f70ce22286c233117047ad2f2378910cd002b2
yosigo777 <103197348+yosigo777@users.noreply.github.com>
2022-04-16 12:42:58 +0800

    Add files via upload

---
+++ 105518125劉育秀_解答2.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 16 12:17:12 2022
+
+@author: 105518125劉育秀
+
+作業二:
+55555
+4444
+333
+22
+1
+
+"""
+for i in range(5,0,-1): #[5,4,3,2,1]
+    for x in range(i):
+        print(i,end="")
+    print()
+
+print("-"*30) #做出一條結束線
+
